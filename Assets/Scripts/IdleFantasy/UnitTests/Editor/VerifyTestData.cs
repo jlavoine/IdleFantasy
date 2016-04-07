4597e2702414864568eb66345de0f347d4e9e2c4
jlavoine <joe.lavoine@gmail.com>
2016-04-07 15:10:23 -0400

    Unit test for verifying that test building is unchanged

---
+++ Assets/Scripts/IdleFantasy/UnitTests/Editor/VerifyTestData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+#pragma warning disable 0414
+
+namespace IdleFantasy.UnitTests {
+    public class VerifyTestData {
+        [Test]
+        public void VerifyTestBuilding() {
+            BuildingData testBuildingData = BuildingLoader.GetData( BuildingLoader.TEST_BUILDING );
+
+            Assert.AreEqual( testBuildingData.ID, "TEST_BUILDING" );
+            Assert.AreEqual( testBuildingData.MaxLevel, 50 );
+            Assert.AreEqual( testBuildingData.Size, 10 );
+            Assert.Contains( new KeyValuePair<string, int>( "Wood", 10), testBuildingData.ResourcesToUpgrade );
+            Assert.Contains( "TEST_CATEGORY", testBuildingData.Categories );
+        }
+    }
+}
