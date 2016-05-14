4d7dfa05428e37b8de42a90cfb9d446fb542b56c
jlavoine <joe.lavoine@gmail.com>
2016-05-14 18:45:01 -0400

    Some integration test progress, but still need more specific tests
    For knowing if a particular endpoint is returning bad data.

--- Assets/Scripts/PlayFab/IntegrationTests/TestForBackendFailure.cs
+++ Assets/Scripts/PlayFab/IntegrationTests/TestForBackendFailure.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MyLibrary;
 
-namespace MyLibrary.PlayFab.IntegrationTests {
+// The purpose of this integration test:
+// 1) Check general login flow to the PlayFab service
+// 2) Make all calls that would be made during gameplay (accessing currencies, player data, etc) to make sure none result in an error
+// TODO: These tests need to be improved...if the actual data is not there, the test still passes because there is no actual error on PlayFab's side.
+// So basically right now this test just checks that PlayFab is working properly.
+
+namespace IdleFantasy.PlayFab.IntegrationTests {
     public class TestForBackendFailure : MonoBehaviour {
 
         private IMessageService mMessenger;
@@ -50,10 +57,28 @@ namespace MyLibrary.PlayFab.IntegrationTests {
         }
 
         private void StartTests() {
-            mBackend.GetVirtualCurrency( "NA", ( result ) => {} );
-            mBackend.GetAllTitleDataForClass( "NA", ( result ) => { } );
-            mBackend.GetPlayerData( "NA", ( result ) => { } );
-            mBackend.GetTitleData( "NA", ( result ) => { } );
+            TestCurrencyCalls();
+            TestPlayerDataCalls();
+            TestTitleDataCalls();
+            TestAllTitleDataCalls();            
+        }
+
+        private void TestCurrencyCalls() {
+            mBackend.GetVirtualCurrency( VirtualCurrencies.GOLD, ( result ) => { } );
+        }
+
+        private void TestPlayerDataCalls() {
+            mBackend.GetPlayerData( PlayerData.BUILDING_PROGRESS, ( result ) => { } );
+            mBackend.GetPlayerData( PlayerData.UNIT_PROGRESS, ( result ) => { } );
+        }
+
+        private void TestTitleDataCalls() {
+            mBackend.GetTitleData( "StringTable_English", ( result ) => { } );
+        }
+
+        private void TestAllTitleDataCalls() {
+            mBackend.GetAllTitleDataForClass( GenericDataLoader.BUILDINGS, ( result ) => { } );
+            mBackend.GetAllTitleDataForClass( GenericDataLoader.UNITS, ( result ) => { } );
         }
     }
 }
