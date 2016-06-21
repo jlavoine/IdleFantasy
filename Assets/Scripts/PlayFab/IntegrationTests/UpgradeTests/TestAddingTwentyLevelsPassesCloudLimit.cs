1728f2fa6a0103a96b6823ed088c2614949d19e0
jlavoine <joe.lavoine@gmail.com>
2016-06-20 20:31:10 -0400

    Twenty calls test for progress levels

---
+++ Assets/Scripts/PlayFab/IntegrationTests/UpgradeTests/TestAddingTwentyLevelsPassesCloudLimit.cs
@@ -0,0 +1,20 @@
+﻿// cloud call limit is 10 calls; adding twenty levels is well beyond that
+
+using System.Collections;
+namespace IdleFantasy.PlayFab.IntegrationTests {
+    public class TestAddingTwentyLevelsPassesCloudLimit : TestPointsUpgrades {
+        private float mProgressToAdd = 20f;
+        private int mExpectedLevel = 21;
+
+        protected override IEnumerator RunTest() {
+            yield return SetStartingSaveLevelAndData( 1 );
+
+            yield return MakeAddProgressCall( mProgressToAdd );
+
+            yield return FailTestIfPointsDoesNotEqual( 0 );
+            yield return FailTestIfNotProgressLevel<GuildProgress>( mCurrentTestData.TestClass, mCurrentTestData.TestID, mExpectedLevel );
+        }
+    }
+}
+
+
