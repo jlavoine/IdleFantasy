a5f789e4f0f235519a2fa993bbdb64b97b6023c6
jlavoine <joe.lavoine@gmail.com>
2016-06-26 20:50:07 -0400

    Adding test to test cloudscript test methods
    Makes sure the test methods are protected by an "isTesting()" clause.

---
+++ Assets/Scripts/PlayFab/IntegrationTests/BasicTests/TestCloudscriptTestMethodsOnlyForTesting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System;
+
+namespace IdleFantasy.PlayFab.IntegrationTests {
+    public class TestCloudscriptTestMethodsOnlyForTesting : IntegrationTestBase {
+        protected override IEnumerator RunAllTests() {
+            foreach ( CloudTestMethods testMethod in Enum.GetValues( typeof( CloudTestMethods ) ) ) {
+                mBackend.MakeCloudCall( testMethod.ToString(), null, null );
+
+                yield return mBackend.WaitUntilNotBusy();
+
+                // this exact thing will probably never happen.
+                // what's more likely to happen is that the test will just fail if the method isn't protected by isTesting()
+                // because the method will try to get arguments that were not passed in.
+                FailTestIfClientInSync( "Test method not protected: " + testMethod.ToString() );
+            }
+        }
+    }
+}
