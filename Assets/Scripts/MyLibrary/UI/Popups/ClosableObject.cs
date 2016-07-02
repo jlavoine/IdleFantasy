fa61c3d585b9d5278494fb78ff7e7e64b2fc7389
jlavoine <joe.lavoine@gmail.com>
2016-07-02 17:52:01 -0400

    Adding generic closable object functionality

---
+++ Assets/Scripts/MyLibrary/UI/Popups/ClosableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MyLibrary {
+    public class ClosableObject : MonoBehaviour {
+
+        public void StartClose() {
+            // TODO: Trigger close animation here, if applicable
+            DestroyThis();
+        }
+
+        private void DestroyThis() {
+            Destroy( gameObject );
+        }
+    }
+}
