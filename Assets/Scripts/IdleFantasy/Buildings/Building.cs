0772be3cb91e4bf92232772d2e81e32955e636df
jlavoine <joe.lavoine@gmail.com>
2016-07-04 14:55:09 -0400

    Buildings were building unit off their data instead of incoming progress

--- Assets/Scripts/IdleFantasy/Buildings/Building.cs
+++ Assets/Scripts/IdleFantasy/Buildings/Building.cs
@@ -63,7 +63,7 @@ namespace IdleFantasy {
             mLevel.UpgradeCompleteEvent += OnUpgraded;
             Level.Value = i_buildingProgress.Level;
 
-            UnitData unitData = GenericDataLoader.GetData<UnitData>( Data.Unit );
+            UnitData unitData = GenericDataLoader.GetData<UnitData>( i_unitProgress.ID );
             Unit = new Unit( unitData, i_unitProgress, mModel );
 
             UpdateCapacity();
