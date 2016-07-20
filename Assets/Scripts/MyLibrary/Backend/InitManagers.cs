5063518b20e62dcac27c882884cee0298c517349
jlavoine <joe.lavoine@gmail.com>
2016-07-19 22:23:19 -0400

    Consolidating all manager instantiation to one file per game

--- Assets/Scripts/MyLibrary/Backend/InitManagers.cs
+++ Assets/Scripts/MyLibrary/Backend/InitManagers.cs
@@ -4,12 +4,14 @@ using MyLibrary;
 #pragma warning disable 0219
 
 namespace IdleFantasy {
-    public class AnalyticsManager : MonoBehaviour {
+    public class InitManagers : MonoBehaviour {
 
         void Awake() {
             DontDestroyOnLoad( this );
 
+            MyLogger logger = new MyLogger();
             PlayFabAnalytics playFab = new PlayFabAnalytics();
+            InfoPopupManager manager = new InfoPopupManager();
         }
     }
 }
