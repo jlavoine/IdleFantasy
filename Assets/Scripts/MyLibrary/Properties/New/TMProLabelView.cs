187d7d92ad7d974b3ff811c7840485ad3c1cb232
jlavoine <joe.lavoine@gmail.com>
2016-09-02 17:30:57 -0400

    Adding a TMProLabelView
    For use with text mesh pro

---
+++ Assets/Scripts/MyLibrary/Properties/New/TMProLabelView.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+
+namespace MyLibrary {
+    public class TMProLabelView : PropertyView {
+        private TextMeshProUGUI mTextField;
+        public TextMeshProUGUI TextField {
+            get {
+                if ( mTextField == null ) {
+                    mTextField = GetComponent<TextMeshProUGUI>();
+                }
+
+                return mTextField;
+            }
+        }
+
+        public override void UpdateView() {
+            object propertyValue = GetValue<object>();
+            string label = propertyValue.ToString();
+
+            if ( TextField != null ) {
+                TextField.SetText( label );
+            }
+            else {
+                MyMessenger.Send<LogTypes, string, string>( MyLogger.LOG_EVENT, LogTypes.Error, "No text element for TMProLabelView: " + PropertyName, "UI" );
+            }
+        }
+    }
+}
