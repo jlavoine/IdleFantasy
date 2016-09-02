a3e1e70df2983637822def866c1c027b7de58c15
jlavoine <joe.lavoine@gmail.com>
2016-09-02 17:30:26 -0400

    Adding map name string method to map name object

--- Assets/Scripts/IdleFantasy/Maps/MapName.cs
+++ Assets/Scripts/IdleFantasy/Maps/MapName.cs
@@ -1,8 +1,19 @@
-﻿
+﻿using MyLibrary;
+
 namespace IdleFantasy {
     public class MapName {
         public MapPieceData Prefix;
         public MapPieceData Terrain;
         public MapPieceData Suffix;
+
+        public string GetStringName() {
+            string nameKey = "_NAME";
+            string prefixString = StringTableManager.Get( Prefix.ID + nameKey );
+            string terrainString = StringTableManager.Get( Terrain.ID + nameKey );
+            string suffixString = StringTableManager.Get( Suffix.ID + nameKey );
+
+            string name = string.Format( "{0} {1} {2}", prefixString, terrainString, suffixString );
+            return name;
+        }
     }
 }
