1f4a955abf5b9b05cac5cfa70a8dcadda57a4d14
jlavoine <joe.lavoine@gmail.com>
2016-09-03 17:24:05 -0400

    Adding unit tests for map area completion and access

--- Assets/Scripts/IdleFantasy/UnitTests/Editor/Map/MapAreaTest.cs
+++ Assets/Scripts/IdleFantasy/UnitTests/Editor/Map/MapAreaTest.cs
@@ -7,7 +7,8 @@ namespace IdleFantasy.UnitTests {
     public class MapAreaTest {
 
         private MapArea mAreaUnderTest;
-        private MapAreaData mMapAreaData;      
+        private MapAreaData mMapAreaData;
+        private SingleMissionProgress mMissionProgress;    
 
         [SetUp]
         public void BeforeTest() {
@@ -17,16 +18,42 @@ namespace IdleFantasy.UnitTests {
             MapData mapData = backend.GetPlayerData_Offline<MapData>( BackendConstants.MAP_BASE );
             mMapAreaData = mapData.Areas[0];
 
-            SingleMissionProgress mockMissionProgress = new SingleMissionProgress();
-            mockMissionProgress.Completed = false;
+            mMissionProgress = new SingleMissionProgress();
+            mMissionProgress.Completed = false;
 
-            mAreaUnderTest = new MapArea( mMapAreaData, mockMissionProgress );
+            mAreaUnderTest = new MapArea( mMapAreaData, mMissionProgress );
         }
 
         [Test]
         public void MapAreaTerrain_MatchesData() {
             string mapAreaTerrainProperty = mAreaUnderTest.ViewModel.GetPropertyValue<string>( MapViewProperties.TERRAIN_TYPE );
             Assert.AreEqual( mapAreaTerrainProperty, mMapAreaData.Terrain.ToString() );
+        }
+
+        [Test]
+        public void MapAreaCompletion_MatchesData() {
+            bool mapAreaCompletion = mAreaUnderTest.ViewModel.GetPropertyValue<bool>( MapViewProperties.AREA_COMPLETED );
+            Assert.AreEqual( mapAreaCompletion, mMissionProgress.Completed );
+        }
+
+        [Test]
+        public void WhenMissionCompleted_AreaNotAccessible() {
+            mMissionProgress.Completed = true;
+            mAreaUnderTest = new MapArea( mMapAreaData, mMissionProgress );
+
+            bool isAccessible = mAreaUnderTest.ViewModel.GetPropertyValue<bool>( MapViewProperties.AREA_ACCESS );
+
+            Assert.IsFalse( isAccessible );
+        }
+
+        [Test]
+        public void WhenMissionNotCompleted_AreaAccessible() {
+            mMissionProgress.Completed = false;
+            mAreaUnderTest = new MapArea( mMapAreaData, mMissionProgress );
+
+            bool isAccessible = mAreaUnderTest.ViewModel.GetPropertyValue<bool>( MapViewProperties.AREA_ACCESS );
+
+            Assert.IsTrue( isAccessible );
         }
     }
 }
