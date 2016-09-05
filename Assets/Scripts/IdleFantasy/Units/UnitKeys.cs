ada028c0c94193be94fe96601996411f5f9eb4c4
jlavoine <joe.lavoine@gmail.com>
2016-09-05 17:58:08 -0400

    Adding some string key files and unit keys
    Also added some basic get methods to UnitKeys

--- Assets/Scripts/IdleFantasy/Units/UnitKeys.cs
+++ Assets/Scripts/IdleFantasy/Units/UnitKeys.cs
@@ -1,6 +1,17 @@
-﻿
+﻿using MyLibrary;
+
 namespace IdleFantasy {
     public static class UnitKeys {
         public const string ICON = "icon_";
+        public const string NAME = "UNIT_NAME_";
+
+        public static string GetIconKey( string i_unitID ) {
+            return ICON + i_unitID;
+        }
+
+        public static string GetName( string i_unitID ) {
+            string key = NAME + i_unitID;
+            return StringTableManager.Get( key );
+        }
     }
 }
---
+++ Assets/Scripts/IdleFantasy/Views/StringKeys.cs
@@ -0,0 +1,8 @@
+﻿
+namespace IdleFantasy {
+    public static class StringKeys {
+        public const string UNLOCK_TEXT = "UNIT_UNLOCKED_TEXT";
+
+        public const string CLASS_KEY = "CLASS";
+    }
+}
