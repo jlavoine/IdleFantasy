975e3156bf3b779306588e58758952d01dd48753
jlavoine <joe.lavoine@gmail.com>
2016-09-13 21:02:01 -0400

    Adding travel options string key

--- Assets/Scripts/IdleFantasy/Views/StringKeys.cs
+++ Assets/Scripts/IdleFantasy/Views/StringKeys.cs
@@ -2,6 +2,7 @@
 namespace IdleFantasy {
     public static class StringKeys {
         public const string UNLOCK_TEXT = "UNIT_UNLOCKED_TEXT";
+        public const string TRAVEL_OPTION_UNAVAILABLE = "TRAVEL_OPTION_UNAVAILABLE";
 
         public const string CLASS_KEY = "CLASS";
     }
