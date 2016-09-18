faa4d1569237c0547e5fe7c3e7f1d8ed5d52f00f
jlavoine <joe.lavoine@gmail.com>
2016-09-17 22:30:34 -0400

    Setting resolution to windowed and 1024x768

--- Assets/Scripts/IdleFantasy/Backend/Startup.cs
+++ Assets/Scripts/IdleFantasy/Backend/Startup.cs
@@ -5,6 +5,8 @@ namespace IdleFantasy {
     public class Startup : MonoBehaviour {
 
         void Start() {
+            Screen.SetResolution( 1024, 768, false );
+
             SceneManager.LoadScene( SceneList.LOGIN );
         }
     }
