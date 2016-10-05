f2a146e04f84007b9cd689b4cd261828dc23ca09
jlavoine <joe.lavoine@gmail.com>
2016-10-04 20:46:06 -0400

    Marking a test user

--- Assets/Scripts/IdleFantasy/Player/TestUsers/TestUsers.cs
+++ Assets/Scripts/IdleFantasy/Player/TestUsers/TestUsers.cs
@@ -6,7 +6,7 @@ namespace IdleFantasy {
         public static string ONE = "76b214b6-7d66-405e-b3b3-e61074984f00";   // 78B0725DE3C139AB
         public static string TWO = "f72a836f-daa4-4b2c-9af8-b94e3e334e46";   // CAA91D6FF24E8D0E
         public static string THREE = "a18d60d3-1c6d-4786-92da-af7f83579a62"; // CC599278F028A100
-        public static string FOUR = "993aa675-b2d6-4aac-a05a-1f1429d1f6d4";
+        public static string FOUR = "993aa675-b2d6-4aac-a05a-1f1429d1f6d4"; // 7D0C8BB44EA7DABF
         public static string FIVE = "29907a5e-ad05-440c-b626-1985ddce7fca";
     }
 }
