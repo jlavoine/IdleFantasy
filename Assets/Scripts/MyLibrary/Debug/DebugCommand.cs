56c3ff921f3dbc742b5704590e6cf1220eb480d0
jlavoine <joe.lavoine@gmail.com>
2016-10-09 20:30:16 -0400

    Adding the basis for a debug panel
    Allows for dragging, adding and executing commands. Scrolls, but clamps
    to the bottom.

---
+++ Assets/Scripts/MyLibrary/Debug/DebugCommand.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MyLibrary {
+    public class DebugCommand {
+        public string Command;
+        public string Description;
+        public Callback<string[]> OnCommand;
+
+        public DebugCommand( string i_commandName, string i_description, Callback<string[]> i_commandCallback ) {
+            Command = i_commandName;
+            Description = i_description;
+            OnCommand = i_commandCallback;
+        }
+    }
+}
