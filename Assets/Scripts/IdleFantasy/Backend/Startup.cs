901380c89e1c905380115c7f91ac5d3cee4fd36e
jlavoine <joe.lavoine@gmail.com>
2016-10-15 20:40:08 -0400

    Changes for building on android

--- Assets/Scripts/IdleFantasy/Backend/Startup.cs
+++ Assets/Scripts/IdleFantasy/Backend/Startup.cs
@@ -5,7 +5,9 @@ namespace IdleFantasy {
     public class Startup : MonoBehaviour {
 
         void Start() {
+            #if UNITY_STANDALONE
             Screen.SetResolution( 1024, 768, false );
+            #endif
 
             SceneManager.LoadScene( SceneList.LOGIN );
         }
