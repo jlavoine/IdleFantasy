3c98428b47b02deb1bbdab650f47548caa3cfaa8
jlavoine <joe.lavoine@gmail.com>
2016-10-18 21:50:08 -0400

    Making game run in background
    This may be causing some bugs?

--- Assets/Scripts/IdleFantasy/Backend/Startup.cs
+++ Assets/Scripts/IdleFantasy/Backend/Startup.cs
@@ -7,6 +7,7 @@ namespace IdleFantasy {
         void Start() {
             #if UNITY_STANDALONE
             Screen.SetResolution( 1024, 768, false );
+            Application.runInBackground = true;
             #endif
 
             SceneManager.LoadScene( SceneList.LOGIN );
